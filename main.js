215c8f796f2ca4d8c60a2805219b9c1ed1f80052
Roogland <akhimierowland@gmail.com>
2023-01-16 11:59:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+let sortedArray = insertionSort([5, 3, 4, 1, 2]);
+console.log(sortedArray); // [1, 2, 3, 4, 5]
